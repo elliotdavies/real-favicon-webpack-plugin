862d8d28ea16c362ac327007dc08f5c073495d0d
Elliot Davies <elliot.a.davies@gmail.com>
2019-03-06 14:30:59 +0000

    Bugfixes for bad logic. Also fix #4 (use new API key)

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ class RealFaviconPlugin {
     this.options = options;
   }
 
+  // Generate and write favicon images
+  generateFavicons(cb) {
+    rfg.generateFavicon(this.request, this.options.outputPath, cb);
+  }
+
   // This is the function Webpack calls during compilation
   apply(compiler) {
     // Hook into https://webpack.js.org/api/compiler-hooks#thiscompilation
+    // for the initial setup
     compiler.hooks.thisCompilation.tap(pluginName, compilation => {
       const jsonPath = path.join(compiler.context, this.options.faviconJson);
       const json = require(jsonPath);
       const outputPath = this.options.outputPath;
 
       const opts = {
-        // https://github.com/RealFaviconGenerator/cli-real-favicon/blob/master/common.js
-        // Seems as though this can just be hard-coded?
-        apiKey: "402333a17311c9aa68257b9c5fc571276090ee56",
+        apiKey: "6ab35684defad6496917dd53a88c9b25dad4f072",
         masterPicture: json.masterPicture,
         iconsPath: json.iconsPath,
         design: json.design,
@@ -35,25 +39,37 @@ class RealFaviconPlugin {
         versioning: json.versioning
       };
 
-      const request = rfg.createRequest(opts);
+      this.request = rfg.createRequest(opts);
 
-      // Hook into https://github.com/jantimon/html-webpack-plugin#beforeemit-hook
-      // as provided by html-webpack-plugin
-      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
-        pluginName,
-        (data, cb) => {
-          if (!this.options.inject) return cb(null, data);
-
-          rfg.generateFavicon(request, this.options.outputPath, (err, res) => {
-            if (err) return cb(err);
-
-            data.html = data.html.replace("</head>", res.favicon.html_code + "</head>");
-
-            cb(null, data);
-          });
-        }
-      );
+      // If we are injecting into some HTML, do the work once html-webpack-plugin is
+      // ready to emit assets https://github.com/jantimon/html-webpack-plugin#beforeemit-hook
+      if (this.options.inject === true) {
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+          pluginName,
+          (data, cb) => {
+            this.generateFavicons((err, res) => {
+              if (err) return cb(err);
+              data.html = data.html.replace(
+                "</head>",
+                res.favicon.html_code + "</head>"
+              );
+              cb(null, data);
+            });
+          }
+        );
+      }
     });
+
+    // If we're not injecting, generate the favicons just before Webpack
+    // emits assets https://webpack.js.org/api/compiler-hooks/#emit
+    if (this.options.inject !== true) {
+      compiler.hooks.emit.tapAsync(pluginName, (compilation, cb) => {
+        this.generateFavicons((err, res) => {
+          if (err) return cb(err);
+          cb(null, compilation);
+        });
+      });
+    }
   }
 }
 
